3f8b4e896792b54ee54a2f4a6cb78c8f5d5faacd
AartiJaiswal <37862199+AartiJaiswal@users.noreply.github.com>
2021-05-11 09:04:33 -0400

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,1 +1,3 @@
-kubectl apply -f "$RootPath\03-allow-ingress-from-istio.yaml"
+#kubectl apply -f "$RootPath\03-allow-ingress-from-istio.yaml"
+$networkPolicyFile = Get-Content -Path "$RootPath\03-allow-ingress-from-istio.yaml" | Out-String
+$networkPolicyFile  | kubectl apply -f -
